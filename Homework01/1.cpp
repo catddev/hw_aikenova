a1953250c47d2c34d4368ca60137fa36472833a7
dinara0708 <aikenova01@gmail.com>
2018-05-21 23:46:11 +0600

    Add project files.

---
+++ Homework01/1.cpp
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+
+
+	int TF = 0, TC = 0;
+	printf("TF= \t");
+	scanf("%d", &TF);
+
+	TC = (TF - 32) * 5 / 9;
+	printf("TC=%d\t", TC);
+
+
+
+
+
+
+	//int A = 0, x = 0, y = 0;
+	//printf("A= \t");
+	//scanf("%d", &A);
+
+	//x = A*A;
+	//printf("%d \n", x);
+
+	//y = x*A;
+	//printf("%d \n", y);
+
+	//x = x*y;
+	//printf("%d \n", x);
+
+	//y = x*x;
+	//printf("%d \n", y);
+
+	//x = x*y;
+	//printf("%d \n", x);
+
+
+
+
+
+	//int A = 0, B = 0;
+	//printf("A= \t");
+	//scanf("%d", &A);
+
+	//B = A*A;
+	//printf("%d \n", B);
+
+	//A = B*B;
+	//printf("%d \n", A);
+
+	//B = A*A;
+	//printf("%d \n", B);
+	//
+
+
+
+
+
+
+	//int x = 0, y = 0;
+	//printf("x= \t");
+	//scanf("%d", &x);
+
+	//y = x - 3;
+	//x = y*y*y;
+	//y = 4 * x*x - 7 * x + 2;
+
+	//printf("y=%d /n", y);
+
+
+
+
+
+
+	//int x = 0, y = 0;
+	//printf("x= \t");
+	//scanf("%d", &x);
+
+	//y = x*x;
+	//x = y*x;
+	//y = 3*x*x - 6*y - 7;
+
+	//printf("y=%d /n", y);
+
+
+
+}
---
+++ Homework01/2..cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+{
+setlocale(LC_ALL, "Rus");
+
+
+
+}
