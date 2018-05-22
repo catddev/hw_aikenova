e191ebc67ce9411c1e6a1088455879797e1892d9
dinara0708 <aikenova01@gmail.com>
2018-05-22 17:26:16 +0600

    -

--- Homework01/1.cpp
+++ Homework01/1.cpp
@@ -7,7 +7,28 @@ void main()
 	setlocale(LC_ALL, "Rus");
 
 
-	int TF = 0, TC = 0;
+	int n = 0;
+	printf("введите номер задачи:\t");
+	scanf("%d", &n);
+
+	if (n == 1)
+
+	{
+		/*Найти значение функции y = 3x^6 - 6x^2 -7 при данном значении x*/
+
+		int x = 0, y = 0;
+		printf("x=");
+		scanf("%d", &x);
+
+		y = x*x;
+		x = y*x;
+		y = 3 * x*x - 6 * y - 7;
+
+		printf("y=%d \n", y);
+	}
+	
+
+	/*int TF = 0, TC = 0;
 	printf("TF= \t");
 	scanf("%d", &TF);
 
@@ -19,72 +40,64 @@ void main()
 
 
 
-	//int A = 0, x = 0, y = 0;
-	//printf("A= \t");
-	//scanf("%d", &A);
+	int A = 0, x = 0, y = 0;
+	printf("A= \t");
+	scanf("%d", &A);
 
-	//x = A*A;
-	//printf("%d \n", x);
+	x = A*A;
+	printf("%d \n", x);
 
-	//y = x*A;
-	//printf("%d \n", y);
+	y = x*A;
+	printf("%d \n", y);
 
-	//x = x*y;
-	//printf("%d \n", x);
+	x = x*y;
+	printf("%d \n", x);
 
-	//y = x*x;
-	//printf("%d \n", y);
+	y = x*x;
+	printf("%d \n", y);
 
-	//x = x*y;
-	//printf("%d \n", x);
-
-
-
-
-
-	//int A = 0, B = 0;
-	//printf("A= \t");
-	//scanf("%d", &A);
-
-	//B = A*A;
-	//printf("%d \n", B);
-
-	//A = B*B;
-	//printf("%d \n", A);
-
-	//B = A*A;
-	//printf("%d \n", B);
-	//
+	x = x*y;
+	printf("%d \n", x);
 
 
 
 
 
+	int A = 0, B = 0;
+	printf("A= \t");
+	scanf("%d", &A);
 
-	//int x = 0, y = 0;
-	//printf("x= \t");
-	//scanf("%d", &x);
+	B = A*A;
+	printf("%d \n", B);
 
-	//y = x - 3;
-	//x = y*y*y;
-	//y = 4 * x*x - 7 * x + 2;
+	A = B*B;
+	printf("%d \n", A);
 
-	//printf("y=%d /n", y);
+	B = A*A;
+	printf("%d \n", B);
+	
 
 
 
 
 
 
-	//int x = 0, y = 0;
-	//printf("x= \t");
-	//scanf("%d", &x);
+	int x = 0, y = 0;
+	printf("x= \t");
+	scanf("%d", &x);
 
-	//y = x*x;
-	//x = y*x;
-	//y = 3*x*x - 6*y - 7;
+	y = x - 3;
+	x = y*y*y;
+	y = 4 * x*x - 7 * x + 2;
 
-	//printf("y=%d /n", y);
+	printf("y=%d /n", y);
+
+*/
+
+
+
+
+
 
 
 
